dfb3613f4970471b9b2a8640eb138bf636d915a8
LeeJiHyeong <mky1168@naver.com>
2021-02-15 17:43:17 +0900

    [ADD] 기본적인 User 도메인 및 DTO 추가

--- src/main/java/com/common/shopping/user/domain/User.java
+++ src/main/java/com/common/shopping/user/domain/User.java
@@ -1,4 +1,57 @@
 package com.common.shopping.user.domain;
 
+import com.sun.istack.NotNull;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicInsert;
+
+import javax.persistence.*;
+import java.util.Calendar;
+
+@NoArgsConstructor
+@Entity
+@DynamicInsert
+@Table(name = "user")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nick_name", unique = true)
+    @NotNull
+    private String nickName;
+
+    @Column(name = "email", unique = true)
+    @NotNull
+    private String email;
+
+    @Column(name = "password")
+    @NotNull
+    private String password;
+
+    @Column(name = "phone")
+    @NotNull
+    private String phone;
+
+    @Column(name = "address")
+    @NotNull
+    private String address;
+
+    @Column(name = "role")
+    @NotNull
+    private Role role;
+
+    @Column(name = "create_date")
+    private Calendar createDate;
+
+    @Builder
+    public User(String nickName, String email, String password, String phone, String address, Role role) {
+        this.nickName = nickName;
+        this.email = email;
+        this.password = password;
+        this.phone = phone;
+        this.address = address;
+        this.role = role;
+    }
 }
--- src/main/java/com/common/shopping/user/dto/UserRegisterDto.java
+++ src/main/java/com/common/shopping/user/dto/UserRegisterDto.java
@@ -1,4 +1,39 @@
 package com.common.shopping.user.dto;
 
+import com.common.shopping.user.domain.Role;
+import com.common.shopping.user.domain.User;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
 public class UserRegisterDto {
+
+    private String nickName;
+    private String email;
+    private String password;
+    private String phone;
+    private String address;
+    private Role role;
+
+    @Builder
+    public UserRegisterDto(String nickName, String email, String password,
+                           String phone, String address, Role role) {
+        this.nickName = nickName;
+        this.email = email;
+        this.password = password;
+        this.phone = phone;
+        this.address = address;
+        this.role = role;
+    }
+
+    public User toEntity() {
+        return User.builder()
+                .nickName(this.nickName)
+                .email(this.email)
+                .password(this.password)
+                .phone(this.phone)
+                .role(this.role)
+                .address(this.address)
+                .build();
+    }
 }
