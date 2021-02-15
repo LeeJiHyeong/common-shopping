df1bc5a1b9dc181463e7d90059de0c842f46cd4f
LeeJiHyeong <mky1168@naver.com>
2021-02-15 15:59:48 +0900

    [ADD] 스키마 SQL 파일 추가

---
+++ src/main/resources/sql.sql
@@ -0,0 +1,52 @@
+CREATE TABLE user (
+    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nick_name VARCHAR(20) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    password VARCHAR(20) NOT NULL,
+    phone VARCHAR(20) NOT NULL,
+    address VARCHAR(100) NOT NULL,
+    role VARCHAR(10) NOT NULL,
+    create_date DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE category (
+    id BIGINT NOT NULL PRIMARY KEY,
+    name VARCHAR (10) NOT NULL
+);
+
+CREATE TABLE product (
+    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+    name VARCHAR (50) AUTO_INCREMENT PRIMARY KEY ,
+    sailer BIGINT,
+    FOREIGN KEY(sailer) REFERENCES user(id)
+);
+
+CREATE TABLE category_product (
+    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+    category BIGINT,
+    product BIGINT,
+    FOREIGN KEY (category) REFERENCES category(id),
+    FOREIGN KEY (product) REFERENCES product(id)
+);
+
+CREATE TABLE sail_post (
+    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    writer BIGINT,
+    product BIGINT,
+    name VARCHAR(255) NOT NULL,
+    content TEXT NOT NULL,
+    create_date DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE review (
+    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    writer BIGINT,
+    content TEXT,
+    image VARCHAR(255),
+    create_date DATETIME DEFAULT CURRENT_TIMESTAMP,
+    sail_post BIGINT NOT NULL,
+    FOREIGN KEY (sail_post) REFERENCES sail_post(id)
+);
+
+
+
