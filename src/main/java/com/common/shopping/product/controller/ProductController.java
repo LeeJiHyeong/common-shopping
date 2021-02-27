b3c879501e6e9d64f9a38feb37d8937fc81cca7d
LeeJiHyeong <mky1168@naver.com>
2021-02-27 15:08:50 +0900

    [UPDATE] validation 의존성 추가

---
+++ src/main/java/com/common/shopping/product/controller/ProductController.java
@@ -0,0 +1,4 @@
+package com.common.shopping.product.controller;
+
+public class ProductController {
+}
---
+++ src/main/java/com/common/shopping/product/service/ProductService.java
@@ -0,0 +1,4 @@
+package com.common.shopping.product.service;
+
+public class ProductService {
+}
